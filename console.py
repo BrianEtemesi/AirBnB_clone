bdf0f8b86f2c347fd07433c0cfa6fe600eb676f0
BrianEtemesi <etemecci@gmail.com>
2023-02-10 15:43:33 +0000

    final `show` method of cmd loop

--- console.py
+++ console.py
@@ -30,6 +30,38 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
+    def do_show(self, arg):
+        """
+        prints the string representation of an instance based
+        on the class name and id
+        """
+        if arg:
+
+            args = arg.split()
+            try:  # check if class name exits
+                cls = globals()[args[0]]
+                if len(args) == 1:
+                    print("** instance id missing **")
+                else:
+                    name_id = args[0] + "." + args[1]
+                    storage.reload()
+                    file_data = storage.all()
+                    for key, value in file_data.items():
+                        if key == name_id:
+                            print(value)
+                        else:
+                            print("** no instance found **")
+            except KeyError:
+                print("** class doesn't exist **")
+                """
+                storage.reload()
+                a = storage.all()
+                for key, value in a.items():
+                    print(value)
+                """
+        else:
+            print("** class name missing **")
+
     def emptyline(self):
         """console to execute nothing when you press enter without an argument"""
         pass
