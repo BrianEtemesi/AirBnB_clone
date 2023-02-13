6aef7e0068b2942635e61feba1ab041623de7e3a
BrianEtemesi <etemecci@gmail.com>
2023-02-13 15:02:12 +0000

    add amenity module to models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.city import City
 from models.state import State
+from models.amenity import Amenity
 import os.path
 import sys
 
